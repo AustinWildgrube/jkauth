b4682a2332d78b55d5f4f3da200fe738d2f3c295
Austin Wildgrube <austinwildgrube@gmail.com>
2020-09-29 16:52:55 -0500

    Add safe URL pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,10 +10,12 @@ import { SlugifyPipe } from './pipes/slugify.pipe';
 
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { QuillModule } from 'ngx-quill';
+import { SafePipe } from './pipes/safe.pipe';
 
 @NgModule({
     declarations: [
-        SlugifyPipe
+        SlugifyPipe,
+        SafePipe
     ],
     imports: [
         RouterModule,
@@ -29,6 +31,7 @@ import { QuillModule } from 'ngx-quill';
         CommonModule,
         TemplatesModule,
         SlugifyPipe,
+        SafePipe,
     ],
     providers: [
         JwtHelperService,
